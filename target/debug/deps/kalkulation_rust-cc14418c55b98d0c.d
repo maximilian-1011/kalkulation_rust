b0dacaa3cd728ee98db142ef3bc3537e683932dd
Maximilian <mbusch1011@gmail.com>
2026-09-25 13:28:16 +0200

    Removed unneccesary folder

--- target/debug/deps/kalkulation_rust-cc14418c55b98d0c.d
+++
@@ -1,9 +0,0 @@
-/home/callus/workspace/rust/kalkulation_rust/target/debug/deps/kalkulation_rust-cc14418c55b98d0c.d: src/main.rs src/kalkulation.rs src/kalkulation/lagerkennzahlen.rs src/kalkulation/beschaffung.rs src/repl.rs
-
-/home/callus/workspace/rust/kalkulation_rust/target/debug/deps/libkalkulation_rust-cc14418c55b98d0c.rmeta: src/main.rs src/kalkulation.rs src/kalkulation/lagerkennzahlen.rs src/kalkulation/beschaffung.rs src/repl.rs
-
-src/main.rs:
-src/kalkulation.rs:
-src/kalkulation/lagerkennzahlen.rs:
-src/kalkulation/beschaffung.rs:
-src/repl.rs:
