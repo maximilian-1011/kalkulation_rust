18911f7ec80b2b74e72544b08e8a034084e59bdb
Maximilian <mbusch1011@gmail.com>
2026-09-26 03:06:53 +0200

    Fixed flaws in argument retrival logic. Implimented Umschlagshäufigkeit, durchschnittlicher Lagerbestand und durchschnittliche Lagerdauer.

--- src/kalkulation/lagerkennzahlen.rs
+++ src/kalkulation/lagerkennzahlen.rs
@@ -3,9 +3,13 @@ pub fn umschlagshäufigkeit(umsatz: f64, durchschnittlicher_lagerbestand: f64) -
     (decimal * 100.0).round() / 100.0
 }
 
-pub fn durchschnittlicher_lagerbestand(anfangsbestand: f64, monatsbestände: Vec<f64>) -> f64 {
-    let sum_of_months: f64 = monatsbestände.iter().sum();
-    let total: f64 = anfangsbestand + sum_of_months;
-    let output: f64 = total / (monatsbestände.len() as f64 + 1.0);
+pub fn durchschnittlicher_lagerbestand(bestände: Vec<f64>) -> f64 {
+    let sum_of_months: f64 = bestände.iter().sum();
+    let output: f64 = sum_of_months / (bestände.len() as f64);
     (output * 100.0).round() / 100.0
+}
+
+pub fn durchschnittliche_lagerdauer(umschlag: f64) -> f64 {
+    let res = 360.0 / umschlag;
+    (res * 100.0).round() / 100.0
 }
--- src/repl.rs
+++ src/repl.rs
@@ -22,8 +22,36 @@ pub fn start_repl() {
                 break;
             }
             Command::Help => call_help(),
-            Command::Umsatzhäufigkeit => break,
-            Command::AvarageStoreTime => break,
+            Command::Umschlagshäufigkeit => {
+                let parms: Vec<&str> = vec!["Umsatz", "Durchschnittliche Lagerdauer"];
+                let val = get_values(parms);
+                let res = lagerkennzahlen::umschlagshäufigkeit(
+                    *val.get("Umsatz").unwrap(),
+                    *val.get("Durchschnittliche Lagerdauer").unwrap(),
+                );
+                println!("\nDas Ergebnis ist: {res}\n");
+                continue;
+            }
+            Command::AvarageStorage => {
+                let parms: Vec<&str> = vec![
+                    "Start", "Jan", "Feb", "März", "April", "Mai", "Juni", "Juli", "Aug", "Sep",
+                    "Okt", "Nov", "Dec",
+                ];
+                let val = get_values(parms);
+                let bestände: Vec<f64> = val.into_values().collect();
+                let res = lagerkennzahlen::durchschnittlicher_lagerbestand(bestände);
+                println!("\nDas Ergebnis ist: {res}\n");
+                continue;
+            }
+            Command::AvarageStoreTime => {
+                let parms: Vec<&str> = vec!["Umschlagshäufigkeit"];
+                let val = get_values(parms);
+                let res = lagerkennzahlen::durchschnittliche_lagerdauer(
+                    *val.get("Umschlagshäufigkeit").unwrap(),
+                );
+                println!("\nDas Ergebnis ist: {res}\n");
+                continue;
+            }
         }
     }
 }
@@ -41,7 +69,8 @@ pub fn clean_up_input(input: &str) -> String {
 enum Command {
     Help,
     Exit,
-    Umsatzhäufigkeit,
+    Umschlagshäufigkeit,
+    AvarageStorage,
     AvarageStoreTime,
 }
 
@@ -75,14 +104,22 @@ fn get_commands() -> HashMap<String, CliCommand> {
         CliCommand {
             name: String::from("um"),
             description: String::from("Brechne Umsatzhäufigkeit"),
-            command: Command::Umsatzhäufigkeit,
+            command: Command::Umschlagshäufigkeit,
+        },
+    );
+    command_map.insert(
+        String::from("dlb"),
+        CliCommand {
+            name: String::from("dlb"),
+            description: String::from("Berechne durchschnittlichen Lagerbestand"),
+            command: Command::AvarageStorage,
         },
     );
     command_map.insert(
         String::from("dld"),
         CliCommand {
             name: String::from("dld"),
-            description: String::from("Berechne durchschnittliche Lagerdauer"),
+            description: String::from("Berechne die durchschnittliche Lagerdauer"),
             command: Command::AvarageStoreTime,
         },
     );
@@ -96,6 +133,7 @@ fn get_values(parms: Vec<&str>) -> HashMap<String, f64> {
     for s in parms {
         print!("{s}: ");
         io::stdout().flush().unwrap();
+        input.clear();
         io::stdin().read_line(&mut input).unwrap();
         let val: f64 = input.trim().parse().unwrap_or(0.0);
         output.insert(String::from(s), val);
